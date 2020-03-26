0da324355d99b4705e9305d8fd4d62143bf9e673
Lucas Yan <38707923+LykanLK@users.noreply.github.com>
2020-03-26 16:11:32 -0300

    Mobile da App

---
+++ mobile2/App.js
@@ -0,0 +1,15 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes';
+
+//nao existe heranca no styles
+
+export default function App() {
+  return (
+    <Routes/>
+  );
+}
